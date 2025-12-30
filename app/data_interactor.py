7da7edd52663f8576c25f87133c96ce18f803147
Shudi <sdbritman@gmail.comgit config --listgit config --listgit config --global user.name>
2025-12-30 14:13:17 +0200

    add create contact func

---
+++ app/data_interactor.py
@@ -0,0 +1,47 @@
+from connection import Singelton, Connector
+
+client = Singelton.get_connection_to_mongo()
+
+def get_collection():
+    contacts_db = Connector.get_database(client)
+    contacts = Connector.get_collection(contacts_db)
+    return contacts
+
+
+def get_all_contacts():
+    col_contacts = get_collection()
+    contacts = col_contacts.find()
+    for contact in contacts:
+        contact["_id"] = str(contact["_id"]) 
+        yield contact
+    return contacts
+    
+
+
+def create_contact(contact_data):
+    col_contacts = get_collection()
+    insert_contacts = col_contacts.insert_one(contact_data)
+    result = col_contacts.find_one({"phone_number": contact_data["phone_number"]})
+    return str(result["_id"])
+
+
+
+def update_contact(contcat_id: str, contact_data: dict):
+    col_contacts = get_collection()
+    doc = {}
+    for v in contact_data.items():
+        if v:
+            doc.update(v)
+    result = col_contacts.update_one(
+        {"id": contcat_id},
+        {"$set":"h"}
+    )
+    return
+
+
+def delete_contact(contact_id: str):
+    col_contacts = get_collection()
+    result = col_contacts.delete_one({'id':contact_id})
+    return True
+
+
