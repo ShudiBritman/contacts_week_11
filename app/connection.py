e1e91815f780f8205a911142e1a15a4bc53d1393
Shudi <sdbritman@gmail.comgit config --listgit config --listgit config --global user.name>
2025-12-31 15:50:50 +0200

    add services yaml files

--- app/connection.py
+++ app/connection.py
@@ -1,12 +1,14 @@
 from pymongo import MongoClient
+import os
 
+db_uri = os.getenv("MONGO_HOST")
 
 class Singelton:
     _instance = None
     @staticmethod
     def get_connection_to_mongo():
         if not Singelton._instance:
-            client = MongoClient('mongodb://localhost:27017')
+            client = MongoClient('mongodb://{db_uri}:27017')
             Singelton._instance = 1
             return client
 
